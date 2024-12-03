a5c0b9562ca20e171a440ecb92c447b1f94d7de8
Jasur <sayfullaevdev22@gmail.com>
2024-12-03 15:41:05 +0500

    Исправил ошибки

--- vite-project/pages/wallets/wallets.js
+++ vite-project/pages/wallets/wallets.js
@@ -1,16 +1,18 @@
-
-import reload from '../../utils/reload.js'; 
+import reload from '../../utils/reload.js';
 import Сard from '../../component/Card.js';
 
-
 const cardContainer = document.getElementById('cardContainer');
+
 async function fetchData() {
     try {
         const response = await fetch('../../db.json');
         const data = await response.json();
 
-        
-        reload(data, Сard, cardContainer);
+        if (Array.isArray(data.wallets)) {
+            reload(data.wallets, Сard, cardContainer);
+        } else {
+            console.error('Свойство wallets отсутствует или не является массивом:', data.wallets);
+        }
     } catch (error) {
         console.error('Ошибка загрузки данных:', error);
     }
